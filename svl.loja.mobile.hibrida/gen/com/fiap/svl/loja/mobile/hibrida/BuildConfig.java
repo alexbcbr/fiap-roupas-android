d19b88f935acdc3d1303d522137f30c39f40547c
alexbc <alexbcbr@gmail.com>
2013-09-17 14:32:51 -0300

    carga inicial

---
+++ svl.loja.mobile.hibrida/gen/com/fiap/svl/loja/mobile/hibrida/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.fiap.svl.loja.mobile.hibrida;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ svl.loja.mobile.hibrida/src/com/fiap/svl/loja/mobile/hibrida/MainActivity.java
@@ -0,0 +1,24 @@
+package com.fiap.svl.loja.mobile.hibrida;
+
+import org.apache.cordova.DroidGap;
+
+import android.os.Bundle;
+import android.view.Menu;
+
+public class MainActivity extends DroidGap {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        super.loadUrl("file:///android_asset/www/formulario.html");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
